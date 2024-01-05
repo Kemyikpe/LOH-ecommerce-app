46e66d45613e95f84fe67502ab504595fdedc191
KemyIkpe <kemyikpe@gmail.com>
2024-01-05 22:48:06 +0100

    home page

---
+++ lib/utils/resources/app_images.dart
@@ -0,0 +1,19 @@
+class AppImage {
+  static const shoeOne =  'assets/images/shoe1.jpg';
+  static const shoeTwo =  'assets/images/shoe2.jpg';
+  static const shoeThree =  'assets/images/shoe3.jpg';
+  static const shoeFour =  'assets/images/shoe4.jpg';
+  static const shoeFive =  'assets/images/shoe5.jpg';
+  static const shoeSix =  'assets/images/shoe6.jpg';
+  static const shoeSeven =  'assets/images/shoe7.jpg';
+  static const shoeEight =  'assets/images/shoe8.jpg';
+  static const shoeNine =  'assets/images/shoe9.jpg';
+  static const shoeTen =  'assets/images/shoe10.jpg';
+  static const belt =  'assets/images/belt.jpg';
+  static const gloves =  'assets/images/gloves.jpg';
+  static const shirt =  'assets/images/shirt.jpg';
+  static const hat =  'assets/images/hat.jpg';
+  static const trouser =  'assets/images/trouser.jpg';
+  static const jewellery =  'assets/images/jewellery.jpg';
+
+}
